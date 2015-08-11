48079afe561bba64d699cc3f16072e1ae86bda3c
BrianSipple <bsipple57@gmail.com>
2015-08-10 21:22:27 -0700

    solution solved

--- index.js
+++ index.js
@@ -3,14 +3,37 @@ var config = require("./config");
 
 var move = actions.move.bind(actions);
 
-function solve(n){
+var towerSolver = (function towerSolver(exports) {
   // fill in solution here
+    
+    function moveDisks(n, startTowerPos, destTowerPos, intermediateTowerPos) {
+        
+        if (n === 1) {        
+            move(startTowerPos, destTowerPos);        
+        } else {
+            moveDisks(n-1, startTowerPos, intermediateTowerPos, destTowerPos);
+            move(startTowerPos, destTowerPos);
+            moveDisks(n-1, intermediateTowerPos, destTowerPos, startTowerPos);
+        }
+    }
+    
+    function solve (n) {
+        
+        var 
+            startTowerPos = 0,
+            intermediateTowerPos = 1,
+            destTowerPos = 2;
+        
+        moveDisks(n, startTowerPos, destTowerPos, intermediateTowerPos);
+    }
 
 
+    return {
+        solve: solve 
+    };
 
 
+}( (typeof exports === 'undefined') ? window : exports));
 
-}
-
-solve(config.pieces);
+towerSolver.solve(config.pieces);
 actions.finish();
